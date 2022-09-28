67ce8df664a6dd6739ae5659f2a723f4ea0637a9
504 <504@DESKTOP-ECPN2C5>
2022-09-28 17:15:44 +0900

    TeamProject_Start

---
+++ TeamProject_Insurance/src/Model/Dao/Dao.java
@@ -0,0 +1,5 @@
+package Model.Dao;
+
+public class Dao {
+	// test
+}
---
+++ TeamProject_Insurance/src/Start/Start.java
@@ -0,0 +1,12 @@
+package Start;
+
+public class Start {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("test");
+		
+		
+	}
+}
