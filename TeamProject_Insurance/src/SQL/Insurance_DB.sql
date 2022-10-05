e32b2db75739b4e5e175692fea239bfa42510041
504 <504@DESKTOP-ECPN2C5>
2022-10-05 15:54:40 +0900

    최종_SQL수정_221005

--- TeamProject_Insurance/src/SQL/Insurance_DB.sql
+++ TeamProject_Insurance/src/SQL/Insurance_DB.sql
@@ -20,7 +20,8 @@ create table board( -- 게시판
     b_content varchar(100) not null,
     b_pw varchar(10) not null,
 	phone varchar(15) not null,
-    foreign key (phone) references member(phone)
+    foreign key (phone) references member(phone) ON DELETE CASCADE
+    
 );
 
 
@@ -33,6 +34,12 @@ create table list( -- 보험 종류 카테고리
     
 ); 
 
+insert into list values( null, "암" );
+insert into list values( null, "손해" );
+insert into list values( null, "치아" );
+-- drop table list;
+-- select * from list;
+
 -- ★★★ age 필드 추가 ★★★
 create table sublist( -- 보험 종류 세부내역 (암)
  
@@ -44,6 +51,10 @@ create table sublist( -- 보험 종류 세부내역 (암)
 	 foreign key (c_num) references list(c_num)
 
 );
+insert into sublist values( null, "폐암", "폐 질환, 폐 수종, 폐기저질환", 1 , 20 );
+insert into sublist values( null, "갑상선암", "갑상선 항진증, 갑상선 치료비", 1 , 30 );
+-- select * from sublist;
+-- drop table sublist;
 
 -- ★★★ 보험 세부내역 테이블 추가 & age 필드 추가 ★★★
 create table sublist2( -- 보험 종류 세부내역 (손해)
@@ -56,6 +67,9 @@ create table sublist2( -- 보험 종류 세부내역 (손해)
 	 foreign key (c_num) references list(c_num)
 
 );
+insert into sublist2 values( null, "상해", "상해 사망, 기타 상해     ", 2 , 20 );
+insert into sublist2 values( null, "질병", "질병 입원비, 질병 수술비", 2 , 30 );
+
 
 -- ★★★ 보험 세부내역 테이블 추가 & age 필드 추가 ★★★
 create table sublist3( -- 보험 종류 세부내역 (치아)
@@ -66,8 +80,9 @@ create table sublist3( -- 보험 종류 세부내역 (치아)
 	 c_num int , 
 	 age int , 
 	 foreign key (c_num) references list(c_num)
-
 );
+insert into sublist3 values( null, "충치", "신경치료, 발치, 크라운", 3 , 20 );
+insert into sublist3 values( null, "임플란트", "고정성, 가공의치(브릿지)", 3 , 30 );
 
 -- ★★★ w_name 에 unique not null 속성 추가 (댓글 기능에 이름만 필요해서 추가함!) ★★★
 create table worker( -- 상담사 카테고리 리스트
@@ -77,6 +92,12 @@ create table worker( -- 상담사 카테고리 리스트
 
 );
 
+-- select * from worker;
+insert into worker values( null , "상담사A");
+insert into worker values( null , "상담사B");
+insert into worker values( null , "상담사C");
+insert into worker values( null , "상담사D");
+
 -- ★★★ w_reply, b_num 필드 추가, phone 필드 삭제 ★★★
 create table subworker( --  상담직원 상세내용 
 
@@ -85,11 +106,11 @@ create table subworker( --  상담직원 상세내용
 	b_num int ,	
 	foreign key (b_num) references board(b_num) ,
     CONSTRAINT subworker_worker_FK foreign key (w_name) references worker(w_name)
-
+	
 );
 
 create table mypage( -- 나의 페이지
-
+	
 	c_num int,
 	-- s_num int,
     phone varchar(15) ,
@@ -99,6 +120,7 @@ create table mypage( -- 나의 페이지
     
 );
 
+-- drop table history;
 -- ★★★ 테이블 신규 추가 ★★★
 create table history( -- 가입 세부내역
 	
